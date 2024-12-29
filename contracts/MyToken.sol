6b8289803943316e90a6a1a57342fc888163c57f
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2024-12-28 20:17:14 -0500

    unzipgit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,32 +3,68 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract OrionKitty is ERC20, Ownable { // Contract name changed
+contract OrionKitty is ERC20, Ownable {
+    using SafeMath for uint256;
+
     uint256 public constant MAX_SUPPLY = 1000000 * 10**18;
+    uint256 public constant MAX_TRANSFER_AMOUNT = 1000 * 10**18;
+    mapping(address => uint256) public lastTransactionTime;
+    uint256 public constant TRANSACTION_COOLDOWN = 15;
 
-    constructor() ERC20("Orion Kitty", "ORKY") Ownable(msg.sender) { // Name and symbol set
+    error TransactionCooldownNotComplete(uint256 remainingCooldown);
+    error TransferAmountExceedsLimit();
+    error TransferToContractOrNonExistentAccount();
+
+    constructor() ERC20("Orion Kitty", "OKTY") Ownable(msg.sender) {
         _mint(msg.sender, MAX_SUPPLY);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {
-        require(to != address(0), "ERC20: transfer to the zero address");
-
+    function _isContract(address account) internal view returns (bool) {
         uint256 size;
-        assembly {
-            size := extcodesize(to)
-        }
-        require(size == 0, "ERC20: transfer to a contract address is not allowed");
+        assembly { size := extcodesize(account) }
+        return size > 0;
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
+    modifier checkTransferLimit(address sender, uint256 amount) {
+        if (amount > MAX_TRANSFER_AMOUNT) {
+            revert TransferAmountExceedsLimit();
+        }
+        _;
+    }
+
+    function transfer(address recipient, uint256 amount) public override checkTransferLimit(_msgSender(), amount) returns (bool) {
+        if (_isContract(recipient)) {
+            revert TransferToContractOrNonExistentAccount();
+        }
+
+        if (msg.sender != owner()) {
+            uint256 timeElapsed = block.timestamp.sub(lastTransactionTime[msg.sender]);
+            if (timeElapsed < TRANSACTION_COOLDOWN) {
+                uint256 remainingCooldown = TRANSACTION_COOLDOWN.sub(timeElapsed);
+                revert TransactionCooldownNotComplete(remainingCooldown);
+            }
+            lastTransactionTime[msg.sender] = block.timestamp;
+        }
+        super.transfer(recipient, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        _spendAllowance(sender, _msgSender(), amount);
-        _transfer(sender, recipient, amount);
+    function transferFrom(address sender, address recipient, uint256 amount) public override checkTransferLimit(sender, amount) returns (bool) {
+        if (_isContract(recipient)) {
+            revert TransferToContractOrNonExistentAccount();
+        }
+
+        if (sender != owner()) {
+            uint256 timeElapsed = block.timestamp.sub(lastTransactionTime[sender]);
+            if (timeElapsed < TRANSACTION_COOLDOWN) {
+                uint256 remainingCooldown = TRANSACTION_COOLDOWN.sub(timeElapsed);
+                revert TransactionCooldownNotComplete(remainingCooldown);
+            }
+            lastTransactionTime[sender] = block.timestamp;
+        }
+        super.transferFrom(sender, recipient, amount);
         return true;
     }
 
